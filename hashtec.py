e3757d9b7af17ddca79032bb935005d1dcdb29da
Paul Hein <pauldhein@email.arizona.edu>
2018-05-07 09:07:01 -0700

    Cleaning up the final project repo

--- hashtec.py
+++ hashtec.py
@@ -1,5 +1,4 @@
 import utils as util
-# from itertools import chain, combinations
 
 
 def compute_vector_table(D):
@@ -34,69 +33,8 @@ def equivClassHashTable(M):
     return list(results.keys())
 
 
-        # pointSets = powerset(points)
-        #
-        # for pointSet in pointSets:
-        #     if len(pointSet) > 1:
-        #         curDict = results
-        #         for i in range(len(pointSet) - 1):
-        #             patPart = util.vectorSubtraction(pointSet[i], pointSet[i+1])
-        #             if patPart not in curDict.keys():
-        #                 curDict[patPart] = {None: (pointSet[0], {(0, 0): 1, vector: 1})}
-        #             else:
-        #                 if curDict[patPart][None][0] == pointSet[0]:
-        #                     curDict[patPart][None][1][vector] = 1
-        #                 elif util.vectorAddition(pointSet[0], vector) == curDict[patPart][None][0]:
-        #                     newVector = tuple(- el for el in vector)
-        #                     curDict[patPart][None][1][newVector] = 1
-        #                 else:
-        #                     pointDiff = util.vectorSubtraction(curDict[patPart][None][0], pointSet[0])
-        #                     newVector = util.vectorAddition(pointDiff, vector)
-        #                     curDict[patPart][None][1][pointDiff] = 1
-        #                     curDict[patPart][None][1][newVector] = 1
-        #             curDict = curDict[patPart]
-
-    # output = dict()
-    # for patPart, topLevelDict in results.items():
-    #     firstPoint = topLevelDict[None][0]
-    #     curPattern = [firstPoint, util.vectorAddition(firstPoint, patPart)]
-    #     (treeMax, newTecs) = buildTECset(0, curPattern, {}, topLevelDict)
-    #     output.update(newTecs)
-    #
-    # return output
-
-
-# def buildTECset(curMax, curPattern, tecs, curDict):
-    # if len(curDict.keys()) == 1:
-    #     translators = list(curDict[None][1].keys())
-    #     return len(translators), {tuple(curPattern): translators}
-    #
-    # iterMax = curMax
-    # for nextPat, newDict in curDict.items():
-    #     if nextPat is not None:
-    #         newPat = curPattern + [util.vectorAddition(curPattern[-1], nextPat)]
-    #         (newMax, newTecs) = buildTECset(curMax, newPat, tecs, newDict)
-    #         tecs.update(newTecs)
-    #         iterMax = max(iterMax, newMax)
-    #
-    # translators = list(curDict[None][1].keys())
-    # if len(translators) > iterMax:
-    #     tecs.update({tuple(curPattern): translators})
-    #     return len(translators), tecs
-    # return iterMax, tecs
-
-
-# def powerset(iterable):
-#     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-#     s = list(iterable)
-#     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
-
 def hashTEC(D):
     V = compute_vector_table(D)
-    # print("V done")
     mHash = mtpHashTable(D, V)
-    # print("mHash done")
-    TECs = equivClassHashTable(mHash)
-    # print("TECs done")
-    return TECs
+    pTypes = equivClassHashTable(mHash)
+    return pTypes
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import utils as util
 from time import time
 from os.path import join
 from os import listdir
-from tqdm import tqdm
-import multiprocessing as mp
 
 from music_parser import XMLParser
 import matplotlib.pyplot as plt
@@ -23,54 +21,7 @@ def syntheticDataTest(dataset):
     startHash = time()
     hashOutput = hashtec.hashTEC(dataset)
     endHash = time()
-    # print("Total time for SIATEC: {}\nTotal time for HashTEC: {} seconds".format(endSia - startSia, endHash - startHash))
-
-    # print("HASH OUTPUT: " + str(hashOutput))
-    # print("SIA OUTPUT: " + str(siaTecOutput))
-    # hashTecOutput = [key for key in hashOutput]
-
-    # equivOutput = util.isEquivTecSets(siaTecOutput, hashTecOutput)
-    #
-    # if not equivOutput:
-    #     util.diagnoseDiff(siaTecOutput, hashTecOutput, dataset)
-
-    # print("HashTEC output:")
-    # for tec in hashTecOutput:
-    #     print(tec)
-
-    # print("SIATEC and HashTEC outputs are equivalent: {} seconds".format(equivOutput))
     return endSia - startSia, endHash - startHash
-
-
-def musicDataTest(filename):
-    piecename = filename[filename.rfind("/") + 1: filename.rfind(".xml")]
-    print("Creating note dataset for {}".format(piecename))
-    parser = XMLParser()
-    (_, parts) = parser.parse_score(filepath=filename)
-    dataset = util.pitch_dataset(parts[0]["melody"])
-    print("Finished note dataset for {}".format(piecename))
-    startSia = time()
-    siaTecOutput = siatec.siatec(dataset)
-    endSia = time()
-    startHash = time()
-    hashOutput = hashtec.hashTEC(dataset)
-    endHash = time()
-    print("Test results for music data from {}".format(piecename))
-    print("Total time for SIATEC: {} seconds\nTotal time for HashTEC: {} seconds".format(endSia - startSia, endHash - startHash))
-
-    # hashTecOutput = [(pat, trans) for pat, trans in hashOutput.items()]
-    # equivOutput = util.isEquivTecSets(siaTecOutput, hashTecOutput, dataset)
-    # print("SIATEC and HashTEC outputs are equivalent: {}".format(equivOutput))
-
-
-def pmap(f, xs):
-    result = list()
-    with mp.Pool(mp.cpu_count()) as p:
-        with tqdm(xs) as pbar:
-            for i, val in tqdm(enumerate(p.imap(f, xs))):
-                result.append(val)
-                pbar.update()
-    return result
 
 
 def densityRuntimeTest():
@@ -82,7 +33,7 @@ def densityRuntimeTest():
 
         siaResults = list()
         hashResults = list()
-        for dataset in tqdm(datasets, desc="Running for d={}".format(density)):
+        for dataset in datasets:
             siaRes, hashRes = syntheticDataTest(dataset)
             siaResults.append(siaRes)
             hashResults.append(hashRes)
@@ -97,60 +48,34 @@ def densityRuntimeTest():
         plt.savefig("prelim_results_{}.png".format(i))
 
 
-def multiLengthMusicTest(datasets, xaxis):
-    siaResults = list()
-    hashResults = list()
-    for dataset in tqdm(datasets, desc="Running over datasets"):
-        siaRes, hashRes = syntheticDataTest(dataset)
-        siaResults.append(siaRes)
-        hashResults.append(hashRes)
-
-    return siaResults, hashResults
-
-    # plt.figure()
-    # plt.title("Dataset size vs Runtime on Synthetic data on music piece")
-    # plt.xlabel("Datasets size")
-    # plt.ylabel("Runtime in seconds")
-    # plt.plot(xaxis, siaResults)
-    # plt.plot(xaxis, hashResults)
-    # plt.legend(["SIATEC", "HashTEC"])
-    # plt.savefig("music_prelim_results.png")
-
-
-def main():
-    # test1 = sorted(synth_tests.regular_dataset)
-    # syntheticDataTest(test1)
-    #
-
+def musicCompTest():
     parker_files = list()
-    # root_dir = join(DROPBOX_MUSICA_XML_ROOT, "./be_bop/")
     music_files = listdir(PARKER_ROOT)
     for filename in music_files:
         if "Charlie Parker" in filename:
             parker_files.append(join(PARKER_ROOT, filename))
 
-    # filename = join(PARKER_ROOT, "be_bop/Charlie Parker - Donna_Lee.xml")
-    # parker_files = parker_files[:2]
-    # siaResults = list()
-    # hashResults = list()
-    # pieceNames = list()
-    # numPieces = len(parker_files)
-    # for filename in parker_files:
-    #     piecename = filename[filename.rfind(" - ") + 3: filename.rfind(".xml")]
-    #     pieceNames.append(piecename)
-    #     print("Creating note dataset for {}".format(piecename))
-    #     parser = XMLParser()
-    #     (_, parts) = parser.parse_score(filepath=filename)
-    #     dataset = util.pitch_dataset(parts[0]["melody"])
-    #     # bounds = np.arange(10, len(dataset) + 10, 10)
-    #     # datasets = [dataset[:int(b)] for b in bounds]
-    #     # siaRes, hashRes = syntheticDataTest(dataset)
-    #     siaTecOutput = siatec.siatec(dataset)
-    #     hashOutput = hashtec.hashTEC(dataset)
-    #     # sia, hash = multiLengthMusicTest(datasets, bounds)
-    #     siaResults.append(len(siaTecOutput))
-    #     hashResults.append(len(hashOutput))
+    filename = join(PARKER_ROOT, "be_bop/Charlie Parker - Donna_Lee.xml")
+    parker_files = parker_files[:2]
+    siaResults = list()
+    hashResults = list()
+    pieceNames = list()
+    numPieces = len(parker_files)
+    parser = XMLParser()
+    for filename in parker_files:
+        piecename = filename[filename.rfind(" - ") + 3: filename.rfind(".xml")]
+        pieceNames.append(piecename)
+        print("Creating note dataset for {}".format(piecename))
 
+        (_, parts) = parser.parse_score(filepath=filename)
+        dataset = util.pitch_dataset(parts[0]["melody"])
+        siaTecOutput = siatec.siatec(dataset)
+        hashOutput = hashtec.hashTEC(dataset)
+        siaResults.append(len(siaTecOutput))
+        hashResults.append(len(hashOutput))
+
+
+def compDensityTest():
     densities = [0.001, 0.01, 0.1]
 
     siaResults = list()
@@ -184,10 +109,12 @@ def main():
     plt.show()
 
 
-    # musicDataTest(filename)
+def main():
+    test1 = sorted(synth_tests.regular_dataset)
+    syntheticDataTest(test1)
 
-    # densityRuntimeTest()
-    plt.show()
+    # musicCompTest()
+    # compDensityTest()
 
 
 if __name__ == '__main__':
